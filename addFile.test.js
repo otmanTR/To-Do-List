8dd1985579c069713185c7230e3be37f84cf4b73
Chris <hakizachrist1@gmail.com>
2023-02-03 00:19:51 +0200

    update addFile

--- addFile.test.js
+++ addFile.test.js
@@ -1,13 +1,18 @@
-const Todolist = require('./addFile.js');
+const Todolist = require('./remove_additem.js');
 // addFunction testing
-const additem = new Todolist;
-const description = 'burak';
-const completed = true;
-const index = 5;
 
 describe('is addFile is working', () => {
+ const additem = new Todolist();
+ const description = 'burak';
+ const completed = true;
+ const index = 1;
   test('add list element', () => {
-    additem.addtask(description, completed, index);
-    expect(additem.toHaveLength(1));
-  });
+   const add = additem.addtask(description, completed, index);
+    expect(add).toHaveLength(1);
+    });
+
+   test('remove list element', () => {
+    const remove = additem.removetask(1);
+     expect(remove).toHaveLength(0);
+    });
 });
