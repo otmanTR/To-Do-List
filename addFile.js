6339c8bccc6d0eaa543745754bf85b288df95ca3
Chris <hakizachrist1@gmail.com>
2023-02-02 20:35:06 +0200

    Update files

--- addFile.js
+++ addFile.js
@@ -4,10 +4,8 @@ class Todolist {
   constructor() {
     this.taskData = [];
   }
-
  static addtask = (description, completed, index) => {
    const newtask = new Tasks(description, completed, index);
-
    this.taskData.push(newtask);
    index = this.taskData.length + 1;
    if (typeof window !== 'undefined') {
