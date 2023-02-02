57cd665f59c9d56acd35c8ef29d392676406d860
Chris <hakizachrist1@gmail.com>
2023-02-02 20:35:16 +0200

    Update files

--- addFile.test.js
+++ addFile.test.js
@@ -1,12 +1,13 @@
 const Todolist = require('./addFile.js');
 // addFunction testing
-
+const additem = new Todolist;
 const description = 'burak';
 const completed = true;
 const index = 5;
 
 describe('is addFile is working', () => {
   test('add list element', () => {
-    expect(Todolist.addtask(description, completed, index).toBe(6));
+    additem.addtask(description, completed, index);
+    expect(additem.toHaveLength(1));
   });
 });
