99e137f994b15b2ca6f4c439b8ad0a21c3e88147
Chris <hakizachrist1@gmail.com>
2023-02-03 17:49:40 +0200

    Update editing test

--- src/js/editing.test.js
+++ src/js/editing.test.js
@@ -1,16 +1,27 @@
 const Todolist = require('./editing.js');
-const Tasks = require('./tasks.js');
 // editing testing
 
 describe('testing editing task', () => {
   const arr = new Todolist();
   let description = 'burak';
-  const completed = true;
+  let completed = true;
   const index = 1;
   let additem = arr.addtask(description, completed, index);
-  test('updating description', () => {
+  completed = false;
+  additem = arr.addtask(description, completed, index);
+ console.log("two item added",additem)
+  test('Updating Description', () => {
     description = "christian";
     const newDescription = arr.updateTask(0, description);
-    expect(newDescription).toBe('christian');
+    expect(newDescription).toBe('christian')
   });
+
+  test('Clear All completed tasks', () => {
+    const newArr = arr.clearAllCompletTask();
+    console.log("arr contain this", newArr);
+    expect(newArr).toHaveLength(1)
+  });
+
+  
+
 });
